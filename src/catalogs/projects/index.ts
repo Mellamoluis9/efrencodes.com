18a969ec538af5a51c22221fdee005593f216622
efrencodes <efrencodes@gmail.com>
2022-11-12 00:14:56 -0600

    ✨ Add catalogs for projects

---
+++ src/catalogs/projects/index.ts
@@ -0,0 +1,61 @@
+import { Project } from "types/projectType"
+
+export const project: Project[] = [
+    {
+		name: 'OCC Mundial',
+		url: 'https://www.occ.com.mx/empresas/',
+		image: {
+			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1658247350/efrencodes.ts/work/occ-mundial.webp',
+			alt: 'Project - OCC Mundial'
+		},
+		tecnologies: ['• ReactJs', '• NextJs', '• Grapqhl']
+	},
+	{
+		name: 'Suzuki México Autos',
+		url: 'https://www.suzuki.com.mx/autos',
+		image: {
+			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1658247333/efrencodes.ts/work/suzuki.webp',
+			alt: 'Project - Suzuki México Autos'
+		},
+		tecnologies: [
+			'• VueJS',
+			'• Quasar Framework',
+			'• HTML/SASS',
+			'• JavaScript',
+			'• Google Maps',
+			'• API Rest'
+		]
+	},
+	{
+		name: 'Bticino México',
+		url: 'https://store.bticino.com.mx/',
+		image: {
+			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1658247340/efrencodes.ts/work/bticino-store.webp',
+			alt: 'Project - Store Bticino México'
+		},
+		tecnologies: [
+			'• VueJS',
+			'• Quasar Framework',
+			'• HTML/SASS',
+			'• API Rest',
+			'• JavaScript',
+			'• Open Pay'
+		]
+	},
+	{
+		name: 'Portafolio Web',
+		url: '#',
+		image: {
+			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1659063057/efrencodes.ts/work/efrencodes.webp',
+			alt: 'Project - Sitio Personal'
+		},
+		tecnologies: [
+			'• Next.js',
+			'• TailwindCSS',
+			'• HTML',
+			'• API Routes',
+			'• JavaScript',
+			'• Markdown'
+		]
+	}
+]
