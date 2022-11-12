d7aa08dd491211984a9059f0f717d482712642e6
efrencodes <efrencodes@gmail.com>
2022-11-12 00:26:37 -0600

    ✨ Add catalogs for metatags, and social media

---
+++ src/catalogs/metatags/index.ts
@@ -0,0 +1,23 @@
+const metaTagsDefault = {
+	title: 'Efrén Martínez 👨🏼‍💻 | Desarrollador Frontend - Vue - React - JavaScript',
+	description:
+		'Desarrollador Frontend especializado en JavaScript. Enfocado en desarrollo web trabajando con tecnologías como Vue y React con TypeScript.',
+	image: 'https://res.cloudinary.com/efrencodes/image/upload/v1658255476/efrencodes.ts/efren-martinez.webp',
+	keywords:
+		'querétaro,tecnologia,software,web,desarrollo,fullstack,software engineer,developer,jamstack,vuejs,reactjs,méxico',
+	canonical: 'https://www.efrencodes.com/',
+	creator: '@efrencodes'
+}
+export const metaTags = {
+	...metaTagsDefault
+}
+
+export const structuredData = {
+	'@context': 'http://schema.org',
+	'@type': 'Person',
+	email: 'mailto:efrencodes@gmail.com',
+	image: 'https://res.cloudinary.com/efrencodes/image/upload/v1654747312/efrencodes.ts/perfil.webp',
+	jobTitle: 'Desarrollador Frontend',
+	name: 'Efrén Martínez',
+	url: 'https://www.efrencodes.com/'
+}
---
+++ src/catalogs/social/index.ts
@@ -0,0 +1,19 @@
+import { Social } from 'types/socialType'
+
+export const socialMedia: Social[] = [
+	{
+		name: 'GitHub',
+		href: 'https://github.com/efrencodes',
+		content: 'Sígueme en github'
+	},
+	{
+		name: 'LinkedIn',
+		href: 'https://www.linkedin.com/in/efrencodes/',
+		content: 'Ponte en contacto'
+	},
+	{
+		name: 'Twitter',
+		href: 'https://twitter.com/efrencodes',
+		content: 'Escríbeme en'
+	}
+]
