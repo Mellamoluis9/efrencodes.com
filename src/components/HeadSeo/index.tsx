430c99364f14078b3bf6d6c595b087ce85fb3fb4
efrencodes <efrencodes@gmail.com>
2022-11-23 17:13:15 -0600

    :sparkles: Add Metatags
    Add data for metatags in head

--- src/components/HeadSeo/index.tsx
+++ src/components/HeadSeo/index.tsx
@@ -1,19 +1,73 @@
+import React, { FC } from 'react'
 import { Head } from 'next/document'
 import { getCssText } from '@stitches'
+import { Props } from './types'
 
-const HeadSeo = () => {
+const HeadSeo: FC<Props> = ({ metaTags, structuredData }) => {
 	return (
 		<Head>
-			<title>Efren Martinez - Frontend Developer</title>
-			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<meta charSet="utf-8" />
+			{structuredData && (
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{
+						__html: JSON.stringify(structuredData)
+					}}
+				/>
+			)}
+			<title>{metaTags.title}</title>
+			<link rel="canonical" href={metaTags.canonical} />
 			<link
-				rel="preconnect"
-				href="https://fonts.gstatic.com"
-				crossOrigin="true"
+				type="text/plain"
+				rel="author"
+				href="https://efrencodes.com/humans.txt"
 			/>
-			<link
-				href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400&display=swap"
-				rel="stylesheet"
+			<meta
+				name="viewport"
+				content="width=device-width,initial-scale=1"
+			/>
+			<meta name="google" content="notranslate" />
+			<meta name="msapplication-TileColor" content="#0099ff" />
+			<meta name="robots" content="index, follow" />
+			<meta name="apple-mobile-web-app-title" content="efrencodes.com" />
+			<meta name="application-name" content="efrencodes.com" />
+			<meta name="theme-color" content="#0099ff" />
+			<meta name="title" content={metaTags.title} />
+			<meta name="description" content={metaTags.description} />
+			<meta name="keywords" content={metaTags.keywords} />
+			<meta name="author" content="Efrén Martínez" />
+			{/* Open Graph / Facebook */}
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={metaTags.canonical} />
+			<meta property="og:title" content={metaTags.title} />
+			<meta property="og:description" content={metaTags.description} />
+			<meta property="og:image" content={metaTags.image} />
+			<meta property="og:image:alt" content={metaTags.title} />
+			<meta property="og:image:width" content="1200" />
+			<meta property="og:image:height" content="630" />
+			<meta property="og:locale" content="es_MX" />
+			<meta property="og:site_name" content="efrencodes.com" />
+			{/* Twitter */}
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:url" content={metaTags.canonical} />
+			<meta property="twitter:title" content={metaTags.title} />
+			<meta
+				property="twitter:description"
+				content={metaTags.description}
+			/>
+			<meta property="twitter:image" content={metaTags.image} />
+			<meta property="twitter:creator" content={metaTags.creator} />
+			<meta property="twitter:site" content={metaTags.creator} />
+			<meta property="twitter:image:alt" content={metaTags.title} />
+			{/* Bing Webmaster Tools */}
+			<meta
+				name="msvalidate.01"
+				content="CFB0EE4F9641D78D3282FD8FAA6D0565"
+			/>
+			{/* ahrefs */}
+			<meta
+				name="ahrefs-site-verification"
+				content="9ad8ba080db7b2dfb9e2af32909749dbec24348bbee6f5aefc5de4e9d28c6b87"
 			/>
 			<style
 				id="stitches"
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
 import NextDocument, { Html, Main, NextScript } from 'next/document'
 import HeadSeo from 'components/HeadSeo'
+import { metaTags, structuredData } from 'catalogs/metatags'
 
 export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang="es">
-				<HeadSeo />
+				<HeadSeo metaTags={metaTags} structuredData={structuredData} />
 				<body>
 					<Main />
 					<NextScript />
