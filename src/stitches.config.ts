8d53b6cde0246307b9e1bc013c30ed41bdd3da12
efrencodes <efrencodes@gmail.com>
2022-12-01 20:43:05 -0600

    ✨ Add radiux-color

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -1,3 +1,4 @@
+import { gray, pink, blue, purple } from '@radix-ui/colors'
 import { createStitches } from '@stitches/react'
 
 export const { styled, getCssText } = createStitches({
@@ -6,23 +7,10 @@ export const { styled, getCssText } = createStitches({
 			sans: '"Inter", sans-serif'
 		},
 		colors: {
-			// background
-			primary: '#2A2A2D',
-			secundary: '#F8F8F8',
-			white: '#fff',
-			black: '#000',
-			// Grey
-			grey900: '#000',
-			grey800: '#191919',
-			grey700: '#323232',
-			grey600: '#4B4B4B',
-			// Intents
-			success100: '#66DDB3',
-			success500: '#66C781',
-			warning100: '#FFD688',
-			warning500: '#FFBB38',
-			danger100: '#FF8C8C',
-			danger500: '#FF4040'
+			...gray,
+			...blue,
+			...pink,
+			...purple
 		},
 		fontSizes: {
 			1: '39px',
