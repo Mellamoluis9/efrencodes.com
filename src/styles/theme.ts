651f2ee31566a3053c369c2f4f7607e5a064617a
efrencodes <efrencodes@gmail.com>
2022-12-25 02:08:52 -0600

    💄 Update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,8 +4,31 @@ const theme = extendTheme({
 	styles: {
 		global: {
 			'html, body': {
-				bg: '#f4f4f5',
-				color: '#15141a'
+				bg: '#F7FAFC',
+				color: '#15141a',
+				fontSize: '14px'
+			},
+			h1: {
+				fontSize: '1.875rem',
+				lineHeight: '1.2',
+				letterSpacing: '-0.03em'
+			},
+			h2: {
+				fontSize: '.8rem',
+				lineHeight: '1.2',
+				letterSpacing: '-0.03em'
+			},
+			h3: {
+				fontSize: '1rem',
+				lineHeight: '1.2',
+				letterSpacing: '-0.02em'
+			},
+			'p, ul, span': {
+				fontSize: '1.125rem',
+				lineHeight: '2',
+				letterSpacing: '-0.01em',
+				fontWeight: 'medium',
+				color: '#5b5b66'
 			}
 		}
 	},
