0a2e221498c1352c03c517862bc7919403a8b4be
efrencodes <efrencodes@gmail.com>
2022-12-26 22:52:50 -0600

    💬 Update text

--- src/catalogs/projects/index.ts
+++ src/catalogs/projects/index.ts
@@ -5,7 +5,7 @@ export const project: Project[] = [
 		name: 'OCC Mundial',
 		url: 'https://www.occ.com.mx/',
 		image: {
-			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1668485401/www.efrencodes.com/project.ts/app-occ-mundial.webp',
+			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1672116525/www.efrencodes.com/project.ts/app-occ-mundial.webp',
 			alt: 'Project - OCC Mundial'
 		},
 		tecnologies: [
@@ -13,13 +13,15 @@ export const project: Project[] = [
 			{ name: 'Nextjs', color: 'purple' },
 			{ name: 'Grapqhl', color: 'orange' },
 			{ name: 'API Rest', color: 'yellow' }
-		]
+		],
+		backgroundImage:
+			'linear-gradient(to right bottom, rgb(255, 255, 255), rgb(248, 250, 252))'
 	},
 	{
 		name: 'Suzuki México Autos',
 		url: 'https://www.suzuki.com.mx/autos',
 		image: {
-			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1668485401/www.efrencodes.com/project.ts/app-suzuki-mexico.webp',
+			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1672116530/www.efrencodes.com/project.ts/app-suzuki-mexico.webp',
 			alt: 'Project - Suzuki México Autos'
 		},
 		tecnologies: [
@@ -27,13 +29,15 @@ export const project: Project[] = [
 			{ name: 'Quasar Framework', color: 'purple' },
 			{ name: 'Google Maps', color: 'pink' },
 			{ name: 'API Rest', color: 'green' }
-		]
+		],
+		backgroundImage:
+			'linear-gradient(to left top, rgba(239, 246, 255, 0.5), rgb(239, 246, 255))'
 	},
 	{
 		name: 'Bticino México',
 		url: 'https://store.bticino.com.mx/',
 		image: {
-			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1668485401/www.efrencodes.com/project.ts/app-bticino-mexico.webp',
+			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1672116522/www.efrencodes.com/project.ts/app-bticino-mexico.webp',
 			alt: 'Project - Store Bticino México'
 		},
 		tecnologies: [
@@ -41,13 +45,15 @@ export const project: Project[] = [
 			{ name: 'Quasar Framework', color: 'pink' },
 			{ name: 'Open Pay', color: 'blue' },
 			{ name: 'API Rest', color: 'orange' }
-		]
+		],
+		backgroundImage:
+			'linear-gradient(to right top, rgb(248, 250, 252), rgb(241, 245, 249))'
 	},
 	{
 		name: 'Search Pokemon',
 		url: 'https://prueba-occ-pokemon.vercel.app/',
 		image: {
-			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1668485401/www.efrencodes.com/project.ts/app-pokemon.webp',
+			src: 'https://res.cloudinary.com/efrencodes/image/upload/v1672116528/www.efrencodes.com/project.ts/app-pokemon.webp',
 			alt: 'Project - Pokemon Search'
 		},
 		tecnologies: [
@@ -55,6 +61,8 @@ export const project: Project[] = [
 			{ name: 'React', color: 'yellow' },
 			{ name: 'NES.css', color: 'cyan' },
 			{ name: 'API Pokemon', color: 'gray' }
-		]
+		],
+		backgroundImage:
+			'linear-gradient(to right bottom, rgba(255, 255, 255, 0.25), rgba(255, 241, 242, 0.5))'
 	}
 ]
