291ed87890facf6b9ca9d4a9e1d8456092717dc0
efrencodes <efrencodes@gmail.com>
2023-02-09 22:11:15 -0600

    💬 Update article in posts

--- src/pages/articulos/[slug].tsx
+++ src/pages/articulos/[slug].tsx
@@ -27,32 +27,34 @@ const Articles = ({ data, content }) => {
 		}
 	}
 	return (
-		<main>
+		<>
 			<Head>
 				<title>{metadataPost.title}</title>
 				<HeadSeo metadata={metadataPost} />
 			</Head>
-			<section className="pt-16 pb-6">
-				<ImageWrapper
-					width={100}
-					height={100}
-					thumbnail={data.cover}
-					alt={data.title}
-				/>
-				<h1 className="text-3xl font-medium text-gray-900 leading-normal pt-4">
-					{data.title}
-				</h1>
-				<p className="italic text-sm pt-2">{data.description}</p>
-			</section>
-			<div
-				className="pt-4 pb-[150px] prose-a:text-pink-600 prose-a:underline prose-h2:text-xl prose-h2:font-medium
+			<main>
+				<section className="pt-16 pb-6">
+					<ImageWrapper
+						width={100}
+						height={100}
+						thumbnail={data.cover}
+						alt={data.title}
+					/>
+					<h1 className="text-3xl font-medium text-gray-900 leading-normal pt-4">
+						{data.title}
+					</h1>
+					<p className="italic text-sm pt-2">{data.description}</p>
+				</section>
+				<div
+					className="pt-4 pb-[150px] prose-a:text-pink-600 prose-a:underline prose-h2:text-xl prose-h2:font-medium
 				prose-h2:tracking-wide prose-h2:text-gray-800 prose-strong:text-gray-900 prose-strong:font-semibold
 				prose-strong:lining-nums prose-ul:list-disc prose-ul:mt-4 prose-ul:mb-4 prose-ul:list-inside prose-li:text-gray-600
 				prose-li:font-normal prose-li:text-base"
-			>
-				<MDXRemote {...content} />
-			</div>
-		</main>
+				>
+					<MDXRemote {...content} />
+				</div>
+			</main>
+		</>
 	)
 }
 
